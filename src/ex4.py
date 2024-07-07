0f92a8ba8af1eed8228c91485077829536d7a290
Sushrut Banerjee <sushrutbanerjee@gmail.com>
2024-07-07 21:55:08 +0530

    excercises 1, 2, 3, 4 all done

---
+++ src/ex4.py
@@ -0,0 +1,8 @@
+color_list = ["red", "black", "green", "blue"]
+index_val1 = int(input("Enter index value for list"))
+index_val2 = int(input("Enter index value for list"))
+
+for item in color_list:
+    print(item)
+ 
+print(color_list[index_val1], color_list[index_val2])
